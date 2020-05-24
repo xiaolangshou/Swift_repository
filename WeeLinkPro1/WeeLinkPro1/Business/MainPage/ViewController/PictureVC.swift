9ed98e35f25fb404368ddbadef410e6256db2a20
xiaolangshou <xiaolangshou@foxmail.com>
2020-05-24 15:30:13 +0800

    add new code

--- WeeLinkPro1/WeeLinkPro1/Business/MainPage/ViewController/PictureVC.swift
+++ WeeLinkPro1/WeeLinkPro1/Business/MainPage/ViewController/PictureVC.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class PictureVC: UIViewController {
 
+    let bottomView = UITextView()
     let scrollView = UIScrollView()
     var data = [1,1,1,1]
     var imageArray: [UIImage] = [UIImage.init(named: "11")!,
@@ -40,18 +41,20 @@ class PictureVC: UIViewController {
             let imgV = UIImageView(frame: CGRect(x: CGFloat(i) * CGFloat(UIScreen.width), y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height))
             imgV.image = imageArray[i]
             scrollView.addSubview(imgV)
-            imgV.tag = i
-            let tap = UITapGestureRecognizer.init(target: self, action: #selector(onTapImage(_:)))
-            imgV.addGestureRecognizer(tap)
-            imgV.isUserInteractionEnabled = true
             imgV.contentMode = .scaleToFill
         }
-    }
-    
-    @objc func onTapImage(_ tap: UITapGestureRecognizer) {
-        if let tag = tap.view?.tag {
-            print(tag)
+        
+        view.addSubview(bottomView)
+        bottomView.snp.makeConstraints { (make) in
+            make.width.equalToSuperview()
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview()
+            make.height.equalTo(100)
         }
+        bottomView.backgroundColor = UIColor.black
+        bottomView.alpha = 0.5
+        bottomView.text = "你好，我的名字叫thomas，请多多指教爱上了的看法建安路快速的减肥啦开始的减肥拉斯科技的分类考试江东父老开始减肥的"
+        bottomView.textColor = UIColor.white
     }
 
 }
