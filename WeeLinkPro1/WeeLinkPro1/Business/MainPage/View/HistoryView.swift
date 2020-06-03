9e3f09d118d3d0d1fe3ad4cce3b41987b959584c
xiaolangshou <xiaolangshou@foxmail.com>
2020-06-03 17:15:27 +0800

    history view modify

--- WeeLinkPro1/WeeLinkPro1/Business/MainPage/View/HistoryView.swift
+++ WeeLinkPro1/WeeLinkPro1/Business/MainPage/View/HistoryView.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class HistoryView: UIView {
     
-    var dataArr = ["铅笔", "小龙虾", "榴莲", "香蕉", "苹果", "梨", "圣女果", "铅笔", "小龙虾", "榴莲", "香蕉", "苹果", "梨", "圣女果"]
+    var dataArr = ["铅笔", "小龙虾", "榴莲", "香蕉", "苹果", "梨", "圣女果", "铅笔", "小龙虾", "榴莲", "香蕉", "苹果", "梨", "圣女果", "铅笔", "lskd", "dsfsdf", "asdfsd", "asdfdf", "aaa"]
     
     let titleLbl = UILabel()
     
@@ -47,30 +47,56 @@ class HistoryView: UIView {
             h.alignment = .center
             h.distribution = .equalCentering
             
-            for j in 0..<(dataArr.count/5) {
+            var preStack: UIStackView?
+            for j in 0..<(dataArr.count/6 + (dataArr.count%6 == 0 ? 0 : 1)) {
                 let stack = UIStackView.init()
                 h.addSubview(stack)
-                stack.snp.makeConstraints { (make) in
-                    make.left.right.equalToSuperview()
-                    make.height.equalTo(60)
+                
+                if let preStack = preStack {
+                    stack.snp.makeConstraints { (make) in
+                        make.left.equalTo(15)
+                        make.height.equalTo(40)
+                        make.top.equalTo(preStack.snp.bottom)
+                        if j == dataArr.count/5-1 {
+                            make.bottom.equalToSuperview()
+                            make.width.equalTo(dataArr.count%6 * 50)
+                        } else {
+                            make.right.equalTo(-15)
+                        }
+                    }
+                } else {
+                    stack.snp.makeConstraints { (make) in
+                        make.left.equalTo(15)
+                        make.right.equalTo(-38)
+                        make.top.equalToSuperview()
+                        make.height.equalTo(40)
+                    }
                 }
+                
                 stack.axis = .horizontal
                 stack.alignment = .center
                 stack.distribution = .equalCentering
+                stack.spacing = 10
                 
-                for i in 0..<5 {
+                for i in 0..<6 {
+                    guard (i + j*6) < dataArr.count else { return }
+                    print(dataArr[i+j*6])
                     let v = UILabel()
-                    v.text = dataArr[i + j]
-                    v.textColor = UIColor.black
-                    v.backgroundColor = UIColor.systemBackColor
-                    v.layer.cornerRadius = 6.0
+                    v.text = dataArr[i + j*6]
+                    v.font = UIFont.PFMedium(11.0)
+                    v.backgroundColor = UIColor.hex(0xF4F4F4)
+                    v.layer.cornerRadius = 4.0
+                    v.clipsToBounds = true
                     stack.addArrangedSubview(v)
                     v.snp.makeConstraints {
-                        $0.width.greaterThanOrEqualTo(60)
+                        $0.width.equalTo(42)
                         $0.height.equalTo(25)
                     }
                     v.textAlignment = .center
+                    v.layoutIfNeeded()
                 }
+                
+                preStack = stack
             }
         }
     }
