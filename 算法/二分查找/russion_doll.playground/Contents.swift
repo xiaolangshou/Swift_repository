0a463f64479ab68679d2e04d330b18d091a39205
xiaolangshou <xiaolangshou@foxmail.com>
2020-08-06 09:58:42 +0800

    russion doll

--- 算法/二分查找/russion_doll.playground/Contents.swift
+++ 算法/二分查找/russion_doll.playground/Contents.swift
@@ -1,8 +1,20 @@
 import UIKit
 
 var envelopes = [[5,4],[6,4],[6,7],[2,3]]
-typealias model = [Int, Int]
+typealias model = [Int]
 
 func russionDoll(arr: [model]) {
+    // w 升序排列, 相同w的h降序
+    var tempArr: [model] = arr
+    tempArr.sort { (s1, s2) -> Bool in
+        if s1.first! == s2.first! {
+            return s1.last! > s2.last!
+        } else {
+            return s1.first! < s2.first!
+        }
+    }
     
+    print(tempArr)
 }
+
+russionDoll(arr: envelopes)
