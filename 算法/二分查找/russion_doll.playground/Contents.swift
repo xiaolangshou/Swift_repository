defedfd5525ade7e693cba34d47c96d4b6a66636
xiaolangshou <xiaolangshou@foxmail.com>
2020-08-06 11:04:44 +0800

    russion doll 2

--- 算法/二分查找/russion_doll.playground/Contents.swift
+++ 算法/二分查找/russion_doll.playground/Contents.swift
@@ -3,18 +3,35 @@ import UIKit
 var envelopes = [[5,4],[6,4],[6,7],[2,3]]
 typealias model = [Int]
 
-func russionDoll(arr: [model]) {
-    // w 升序排列, 相同w的h降序
-    var tempArr: [model] = arr
-    tempArr.sort { (s1, s2) -> Bool in
-        if s1.first! == s2.first! {
-            return s1.last! > s2.last!
+func sortArray(_ envelopes: [[Int]]) -> [[Int]] {
+    var tempEnvelopes = envelopes
+    tempEnvelopes = tempEnvelopes.sorted { (s1, s2) -> Bool in
+        if (s1.first ?? 0 == s2.first ?? 0) {
+            return s1.last ?? 0 > s2.last ?? 0
         } else {
-            return s1.first! < s2.first!
+            return s1.first ?? 0 < s2.first ?? 0
+        }
+    }
+    print("========envelopes==\(tempEnvelopes)")
+    return tempEnvelopes
+}
+
+func maxEnvelopes(_ envelopes: [[Int]]) -> Int {
+    guard envelopes.count > 1 else { return envelopes.count }
+    let sortEnvelopes = sortArray(envelopes)
+
+    var countArr = Array.init(repeating: 1, count: envelopes.count)
+    for i in 0..<sortEnvelopes.count {
+        for j in 0..<i {
+            if sortEnvelopes[i][1] > sortEnvelopes[j][1] {
+                print("\(countArr[i]),\(countArr[j]+1)")
+                countArr[i] = max(countArr[i], countArr[j]+1)
+            }
         }
     }
     
-    print(tempArr)
+    print("\(countArr)")
+    return countArr.max()!
 }
 
-russionDoll(arr: envelopes)
+print(maxEnvelopes(envelopes))
