bc9e076d4716b332a3bc3515f2bf82ce0588b90b
xiaolangshou <xiaolangshou@foxmail.com>
2020-08-27 16:35:36 +0800

    add new code for ffmpeg

---
+++ LBffmpegDemo-master/LBffmpegDemo/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  LBffmpegDemo
+//
+//  Created by gold on 17/7/7.
+//  Copyright © 2017年 Bison. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "LBffmpegTool.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    //解码
+//    NSString *info_ns = [[LBffmpegTool sharedInstance] decoder:@"resource.bundle/sintel.mov" output_str:@"resource.bundle/test1.yuv"];
+//    NSLog(@"解码后的信息%@",info_ns);
+    /*
+     *在运行在真机的代码中必须把地址换成你自己的电脑IP（10.10.10.134）（不能再用local 本地了）
+     *运行服务器的电脑和手机保证在同一WiFi下
+     */
+    
+    // 推流
+    [[LBffmpegTool sharedInstance] pushFlow:@"resource.bundle/war3end.mp4" output_str:@"rtmp://weelinktech.xyz/live/lt?txSecret=26ba5e91edc4d7e98ac7b333c98344ef&txTime=5F48A725"];
+    // FFmpeg获取摄像头设备
+//    [[LBffmpegTool sharedInstance] showDevice];
+//    [[LBffmpegTool sharedInstance] getMovieDevice: self.view];
+}
+
+@end
