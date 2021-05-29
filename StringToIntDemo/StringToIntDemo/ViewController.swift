cba8f80d2b1f69c451a12ab5036c2bc9506585f6
xiaolangshou <xiaolangshou@foxmail.com>
2021-05-29 10:07:14 +0800

    add new demos

---
+++ StringToIntDemo/StringToIntDemo/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  StringToIntDemo
+//
+//  Created by lian shan on 2021/5/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let stepCount = "asdfsd"
+
+        print(Int(stepCount) ?? 0)
+    }
+
+
+}
+
